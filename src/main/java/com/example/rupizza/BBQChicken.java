b3e8ef733222dbffe9720da39571acb836f04990
RobJ1925 <robj1925@gmail.com>
2022-11-10 18:49:49 -0500

    testing

---
+++ src/main/java/com/example/rupizza/BBQChicken.java
@@ -0,0 +1,19 @@
+package com.example.rupizza;
+
+public class BBQChicken extends Pizza{
+    @Override
+    public boolean add(Object obj) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object obj) {
+        return false;
+    }
+
+    @Override
+    public double price() {
+        return 0;
+    }
+}
+
---
+++ src/main/java/com/example/rupizza/Customizable.java
@@ -0,0 +1,6 @@
+package com.example.rupizza;
+
+public interface Customizable<E> {
+    boolean add(Object obj);
+    boolean remove(Object obj);
+}
